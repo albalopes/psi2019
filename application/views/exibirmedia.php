680692d11cc5696ca8bb13963201c450e8591db7
Alba Sandyra Bezerra Lopes <2813232@ifrn.local>
2019-06-13 15:43:07 -0300

    Aula do dia 13/06/2019

---
+++ application/views/exibirmedia.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>
+		</title>
+	</head>
+
+	<body>
+		<h2>Sua média é: 
+			<?php echo $media; ?></h2>
+	</body>
+</html>
---
+++ application/views/listaralunos.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>
+		</title>
+	</head>
+
+	<body>
+		<h2>Listagem de alunos</h2>
+		<ul>
+			<li>Diogo</li>
+			<li>Wesliana</li>
+			<li>Gabriel</li>
+			<li>Juliana</li>
+			<li>Cibele</li>
+			<li>Scheylla</li>
+		</ul>
+	</body>
+</html>
